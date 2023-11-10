69b3bbd28e1459b80f2a1b4bc7ff12d36d0d40ab
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-10 13:48:33 +0300

    Added Arrays and Strings class to demonstrate Arrays and Strings concepts.

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,9 +1,16 @@
 package arrays;
 
 public class Main {
-    //Arrays and Strings
 
+    public static void main(String[] args) {
+        //Arrays and Strings
+        int x;
+        int[] sayilar = {1, 3, 5, 7, 9};
+        Object obj = new Object();
+        double[] notes;
+        double[] phyGrades = new double[100];
 
+    }
 
 
 }
