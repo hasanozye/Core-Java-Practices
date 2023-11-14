ae8691603866e4873197c31bad247dce8858513a
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-14 20:25:26 +0300

    Implement Array667 class to count the occurrences of two consecutive 6's or a 6 followed by 7 in an array of integers.

---
+++ src/arrays/Array667.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Array667 {
+    public static void main(String[] args) {
+        int[] array1 = {6, 6, 2};
+        int[] array2 = {6, 6, 2, 6};
+        int[] array3 = {6, 7, 2, 6};
+
+        System.out.println(array667(array1)); // Output: 1
+        System.out.println(array667(array2)); // Output: 1
+        System.out.println(array667(array3)); // Output: 1
+    }
+
+    public static int array667(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
