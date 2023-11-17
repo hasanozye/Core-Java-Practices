bb60514179d340726eed45fad0fe88a56b78fc3f
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-17 23:37:59 +0300

    Implement Java class WithoutEnd2 with a method to return a version of the input string without both the first and last characters, along with test cases for validation.

---
+++ src/strings/WithoutEnd2.java
@@ -0,0 +1,21 @@
+package strings;
+
+public class WithoutEnd2 {
+    public static void main(String[] args) {
+        System.out.println(withoutEnd2("Hello"));  // Output: "ell"
+        System.out.println(withoutEnd2("abc"));    // Output: "b"
+        System.out.println(withoutEnd2("ab"));     // Output: ""
+    }
+
+    public static String withoutEnd2(String str) {
+        // Check if the string has at least two characters
+        if (str.length() >= 2) {
+            // Return the substring excluding the first and last characters
+            return str.substring(1, str.length() - 1);
+        } else {
+            // If the string has fewer than two characters, return an empty string
+            return "";
+        }
+    }
+}
+
