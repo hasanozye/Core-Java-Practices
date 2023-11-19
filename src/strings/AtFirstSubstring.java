696d667cd36e465ae336ebc3d0888b356aeeec1b
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-19 18:22:20 +0300

    Add AtFirstSubstring class in the strings package with a method atFirst that returns a string containing the first 2 characters of the input string or '@@' if empty, and '@' followed by '@' if the input string has a length of 1. Includes examples in the main method demonstrating its usage.

--- src/strings/AtFirstSubstring.java
+++ src/strings/AtFirstSubstring.java
@@ -2,14 +2,8 @@ package strings;
 
 public class AtFirstSubstring {
     public static String atFirst(String str) {
-        // Check if the string length is at least 2
-        if (str.length() >= 2) {
-            // If so, return the first 2 characters
-            return str.substring(0, 2);
-        } else {
-            // If the string length is less than 2, use '@' for the missing characters
-            return str.length() == 1 ? str + "@" : "@@";
-        }
+
+        return str.length()>=2 ? str.substring(0,2) : str.isEmpty() ? "@@" : str+"@";
     }
 
     public static void main(String[] args) {
@@ -21,6 +15,8 @@ public class AtFirstSubstring {
 
         // Example 3
         System.out.println(atFirst("h"));  // Output: "h@"
+
+        
     }
 }
 
