247e40038faed9f57f80bb6030a537600e7dd41a
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-20 18:50:52 +0300

    Implemented a Java class 'ColorExtractor' containing a method 'seeColor' that, given a string, returns the color ('red' or 'blue') if the string starts with it, otherwise returns an empty string. Included example usage in the main method.

---
+++ src/strings/ColorExtractor.java
@@ -0,0 +1,21 @@
+package strings;
+
+public class ColorExtractor {
+    public static String seeColor(String s) {
+        if (s.startsWith("red")) {
+            return "red";
+        } else if (s.startsWith("blue")) {
+            return "blue";
+        } else {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        System.out.println(seeColor("redxx"));      // Output: "red"
+        System.out.println(seeColor("xxred"));      // Output: ""
+        System.out.println(seeColor("blueTimes"));  // Output: "blue"
+    }
+}
+
