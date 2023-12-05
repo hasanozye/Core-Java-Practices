9a2b0a35bfe21f0a0a666d7a88c0a06827d6fc6b
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-05 17:50:00 +0300

    "Implemented a Java class with a method to calculate the sum of elements in an integer array of dynamic size using a for loop, offering flexibility for arrays of varying lengths.

---
+++ src/arrays/sum3.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class sum3 {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {5, 11, 2};
+        int[] array3 = {7, 0, 0};
+
+        System.out.println(sumArray(array1)); // Output: 6
+        System.out.println(sumArray(array2)); // Output: 18
+        System.out.println(sumArray(array3)); // Output: 7
+    }
+
+    public static int sumArray(int[] nums) {
+        int sum = 0;
+
+        // Loop through the array and add each element to the sum
+        for (int num : nums) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
