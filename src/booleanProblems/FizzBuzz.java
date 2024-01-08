9ddee57cbc9e1e0477979d5b8e2d6823ea195ff0
Hasan Özyer <hasanozyer06@gmail.com>
2024-01-08 11:10:34 +0300

    solved fizzbuzz

---
+++ src/booleanProblems/FizzBuzz.java
@@ -0,0 +1,25 @@
+package booleanProblems;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        System.out.println(fizzString("fig"));  // Output: "Fizz"
+        System.out.println(fizzString("dib"));  // Output: "Buzz"
+        System.out.println(fizzString("fib"));  // Output: "FizzBuzz"
+    }
+
+    public static String fizzString(String str) {
+        boolean startsWithF = str.startsWith("f");
+        boolean endsWithB = str.endsWith("b");
+
+        if (startsWithF && endsWithB) {
+            return "FizzBuzz";
+        } else if (startsWithF) {
+            return "Fizz";
+        } else if (endsWithB) {
+            return "Buzz";
+        } else {
+            return str;
+        }
+    }
+}
