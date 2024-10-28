1aa8a40fe5dc76da1e3798855d2f1f8acb14bee9
hasan.ozyer <hasanozyer06@gmail.com>
2024-10-28 13:44:27 +0300

    main class for running my methods.

---
+++ src/AdvancedConcepts/OOP/Program.java
@@ -0,0 +1,34 @@
+package AdvancedConcepts.OOP;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Dog k1 = new Dog();
+        k1.type = "Kangal";
+        k1.age = 3;
+        k1.weight = 11.1;
+        k1.bilgiGoster();
+
+        System.out.println();
+        System.out.println("--".repeat(20));
+
+        Dog k2 = new Dog();
+        k2.type = "Doberman";
+        k2.age = 1;
+        k2.weight = 3.5;
+        k2.bilgiGoster();
+
+        System.out.println();
+        System.out.println("--".repeat(20));
+
+        Car car = new Car();
+        car.color = "RED";
+        System.out.println("car.color = " + car.color);
+
+
+    }
+
+}
