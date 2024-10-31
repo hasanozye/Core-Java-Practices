55a5d1981a9301417cf71030b9efad1a49805300
hasan.ozyer <hasanozyer06@gmail.com>
2024-10-31 17:58:57 +0300

    mainapp for executing my object classes

--- src/AdvancedConcepts/OOP2/TrainingStudent/MainApp.java
+++ src/AdvancedConcepts/OOP2/TrainingStudent/MainApp.java
@@ -3,7 +3,6 @@ package AdvancedConcepts.OOP2.TrainingStudent;
 public class MainApp {
 
     public static void main(String[] args) {
-
         Student student = new Student();
         student.firstName = "Ahmet";
         student.lastName = "Dursun";
