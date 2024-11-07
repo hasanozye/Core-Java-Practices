2e878ccccab71ca89bf7f95fdded803594ce6310
hasan.ozyer <hasanozyer06@gmail.com>
2024-11-07 20:02:30 +0300

    MainApp for main app thingz

---
+++ src/AdvancedConcepts/OOP6/MainApp.java
@@ -0,0 +1,16 @@
+package AdvancedConcepts.OOP6;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Hayvan hy1 = new Hayvan();
+        Kedi k1 = new Kedi();
+
+        hy1.beslen();
+        k1.beslen();
+        k1.avlan();
+
+    }
+
+}
