7886483fb132bb0ad2daa9d11f8408812bc27c33
hasan.ozyer <hasanozyer06@gmail.com>
2024-11-07 20:02:18 +0300

    hayvan kedi sınıfları oluşturuldu

---
+++ src/AdvancedConcepts/OOP6/Hayvan.java
@@ -0,0 +1,14 @@
+package AdvancedConcepts.OOP6;
+
+public class Hayvan {
+
+
+    public Hayvan(){
+        System.out.println("Hayvan nesnesi oluşturuldu.");
+    }
+
+    public void beslen(){
+        System.out.println("Hayvan besleniyor.");
+    }
+
+}
---
+++ src/AdvancedConcepts/OOP6/Kedi.java
@@ -0,0 +1,18 @@
+package AdvancedConcepts.OOP6;
+
+public class Kedi extends Hayvan {
+
+    public Kedi() {
+        System.out.println("Kedi nesnesi oluşturuldu.");
+    }
+
+    public void avlan() {
+        System.out.println("Kedi avlanıyor.");
+    }
+
+    @Override
+    public void beslen() {
+        System.out.println("Kedi caiz e");
+    }
+
+}
